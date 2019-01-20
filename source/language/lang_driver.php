6c68ede65d5ccac2fcda285536806e77baf14738
seagle <jby0773@163.com>
2019-01-20 18:50:30 +0800

    template up

---
+++ source/language/lang_driver.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_template.php 34422 2014-04-23 09:56:17Z nemohou $
+ *
+ *      This file is automatically generate
+ */
+
+$lang = array (
+  'test' => '测试',
+  'register' => '注册',
+);
+
+?>
